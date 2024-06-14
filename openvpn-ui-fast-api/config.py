3249e133b87a9fedb99a7d863069a169c6d1aa2c
eternityduck <bogdan2003bk96@gmail.com>
2024-06-14 22:05:12 +0300

    rename mgmt_host var

--- openvpn-ui-fast-api/config.py
+++ openvpn-ui-fast-api/config.py
@@ -20,7 +20,7 @@ OPENVPN_LISTEN_HOST = os.getenv("OPENVPN_LISTEN_HOST", "127.0.0.1")
 OPENVPN_LISTEN_PORT = try_parse_int(os.getenv("OPENVPN_LISTEN_PORT")) or 1194
 OPENVPN_PROTOCOL = os.getenv("OPENVPN_PROTOCOL", "tcp")
 OPENVPN_CCD_PATH = os.getenv("OPENVPN_CCD_PATH", "../ccd")
-OPENVPN_MGMT_HOST = os.getenv("OPENVPN_MGMT_INTERFACE", "openvpn")
+OPENVPN_MGMT_HOST = os.getenv("OPENVPN_MGMT_HOST", "openvpn")
 OPENVPN_MGMT_PORT = try_parse_int(os.getenv("OPENVPN_MGMT_PASSWORD")) or 8989
 OPENVPN_PASSWORD_AUTH = os.getenv("OPENVPN_PASSWORD_AUTH", False) == "true"
 SQLITE_DB_PATH = os.getenv("SQLITE_DB_PATH", "/opt/db/openvpn-ui.db")
